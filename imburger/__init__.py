26f80d774b588d097f35aa09c944cea3f1aaef5b
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-07 10:28:57 -0300

    App setup

--- imburger/__init__.py
+++ imburger/__init__.py
@@ -1,8 +1,23 @@
+import os
+
 from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get('DB_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///imburger.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
-def index():
-    return "Welcome to IMBurger"
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from imburger import routes
