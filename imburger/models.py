265d128ce4cc1e2671063c4336465c4d69f337e5
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-06-05 15:43:27 -0300

    Some fixing

--- imburger/models.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
-from imburger import db, login_manager
-from flask_login import UserMixin
