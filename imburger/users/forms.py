643685667805f2fbce39319f12c9edeaa6ae466c
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-06-08 11:46:02 -0300

    Starting routes and forms

--- imburger/users/forms.py
+++ imburger/users/forms.py
@@ -1,6 +1,46 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import (
+    DataRequired, Length, Email, EqualTo, ValidationError
+)
 from flask_login import current_user
-from flaskblog.models import User
+
+from imburger.models import User, Customer, Employee
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=2, max=20)])
+    given_name = StringField('Nome', validators=[
+        DataRequired(), Length(min=2, max=20)])
+    surname = StringField('Sobrenome', validators=[
+        DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[
+        DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password')])
+    registration_number = StringField('Matrícula de funcionário', validators=[
+        Length(min=2, max=20)])
+    street = StringField('Nome da rua', validators=[
+        DataRequired(), Length(min=2, max=100)])
+    number = StringField('Número', validators=[
+        DataRequired(), Length(min=2, max=10)])
+    zipcode = StringField('CEP', validators=[
+        Length(min=2, max=10)])
+    neighborhood = StringField('Bairro', validators=[
+        Length(min=2, max=100)])
+    city = StringField('Cidade', validators=[
+        Length(min=2, max=100)])
+    submit = SubmitField('Cadastrar')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username has already been used')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email has already been used')
--- imburger/users/routes.py
+++ imburger/users/routes.py
@@ -1,13 +1,40 @@
-from flask import render_template, url_for, flash, redirect, request, Blueprint
-from flask_login import login_user, current_user, logout_user, login_required
+from flask import (
+    render_template, url_for, flash, redirect, request, Blueprint
+)
+from flask_login import (
+    login_user, current_user, logout_user, login_required
+)
+
 from imburger import db, bcrypt
+from imburger.models import User, Employee, Customer
+from imburger.users import forms
+
 
 users = Blueprint('users', __name__)
 
-
-@users.route("/register", methods=['GET', 'POST'])
+@users.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Registrar Conta')
+    if current_user.is_authenticated:
+        flash('You are already logged in!', 'warning')
+        return redirect(url_for('main.home'))
+
+    form = forms.RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            given_name=form.given_name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been successfully created!', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @users.route("/login", methods=['GET', 'POST'])
