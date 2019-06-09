419fc3d4477215a966c374dfd4adfecc6ac7b05c
Lucas Novaes <lucasmsn200@gmail.com>
2019-06-09 17:40:12 -0300

    adicionando funcionalidade de criar usuario

--- imburger/config.py
+++ imburger/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'youremail@gmail.com'
+    MAIL_PASSWORD = 'yourpassword'
--- imburger/users/forms.py
+++ imburger/users/forms.py
@@ -11,44 +11,44 @@ from imburger.models import User, Customer, Employee
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
-        DataRequired(), Length(min=2, max=20)])
+        DataRequired(message="Campo obrigatorio"), Length(min=2, max=20, message="Seu username precisa ter entre 2 e 20 caracteres")])
     given_name = StringField('Nome', validators=[
-        DataRequired(), Length(min=2, max=20)])
+        DataRequired(message="Campo obrigatorio"), Length(min=2, max=20, message="Seu nome precisa ter entre 2 e 20 caracteres")])
     surname = StringField('Sobrenome', validators=[
-        DataRequired(), Length(min=2, max=20)])
+        DataRequired(message="Campo obrigatorio"), Length(min=2, max=20, message="Seu sobrenome precisa ter entre 2 e 20 caracteres")])
     email = StringField('Email', validators=[
-        DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+        DataRequired(message="Campo obrigatorio"), Email(message="email invalido")])
+    password = PasswordField('Password', validators=[DataRequired(message="Campo obrigatorio")])
     confirm_password = PasswordField('Confirm Password', validators=[
-        DataRequired(), EqualTo('password')])
+        DataRequired(message="Campo obrigatorio"), EqualTo('password', message="Senhas digitadas não são iguais")])
     registration_number = StringField('Matrícula de funcionário', validators=[
-        Length(min=2, max=20)])
+        Length(min=2, max=20, message="Este campo precisa ter entre 2 e 20 caracteres")])
     street = StringField('Nome da rua', validators=[
-        DataRequired(), Length(min=2, max=100)])
+        DataRequired(message="Campo obrigatorio"), Length(min=2, max=100, message="Este campo precisa ter entre 2 e 100 caracteres")])
     number = StringField('Número', validators=[
-        DataRequired(), Length(min=2, max=10)])
+        DataRequired(message="Campo obrigatorio"), Length(min=2, max=10, message="Este campo precisa ter entre 2 e 10 caracteres")])
     zipcode = StringField('CEP', validators=[
-        Length(min=2, max=10)])
+        Length(min=2, max=10, message="Este campo precisa ter entre 2 e 10 caracteres")])
     neighborhood = StringField('Bairro', validators=[
-        Length(min=2, max=100)])
+        Length(min=2, max=100, message="Este campo precisa ter entre 2 e 100 caracteres")])
     city = StringField('Cidade', validators=[
-        Length(min=2, max=100)])
-    submit = SubmitField('Cadastrar')
+        Length(min=2, max=100, message="Este campo precisa ter entre 2 e 100 caracteres")])
+    submit = SubmitField('Criar conta')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username has already been used')
+            raise ValidationError('Este username já está em uso')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email has already been used')
+            raise ValidationError('Este email já está em uso')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[
-        DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember me')
+        DataRequired(message="Campo obrigatorio"), Email()])
+    password = PasswordField('Password', validators=[DataRequired(message="Campo obrigatorio")])
+    remember = BooleanField('Lembre-me')
     submit = SubmitField('Login')
